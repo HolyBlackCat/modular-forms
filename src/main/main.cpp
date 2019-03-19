153f402693aa5fd2da5e444a04c93a0781a78952
HolyBlackCat <blckcat@inbox.ru>
2019-03-19 21:09:05 +0300

    Imgui window prototype.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,17 +1,67 @@
 #include <iostream>
 
-#include <imgui.h>
-#include <imgui_impl_opengl2.h>
-#include <imgui_impl_sdl.h>
-
 #include "interface/window.h"
 #include "graphics/complete.h"
 #include "program/exit.h"
 #include "utils/clock.h"
 #include "utils/dynamic_storage.h"
+#include "utils/mat.h"
 #include "utils/metronome.h"
 
+#include <imgui.h>
+#include <imgui_freetype.h>
+#include <imgui_impl_opengl2.h>
+#include <imgui_impl_sdl.h>
+
 Interface::Window window;
+
+namespace VisualOptions
+{
+    constexpr int
+        outer_margin = 16;
+
+    constexpr float
+        step_list_width_relative = 0.2,
+        step_list_min_width_pixels = 30,
+        step_list_max_width_relative = 0.5;
+
+    void GuiStyle(ImGuiStyle &style)
+    {
+        ImGui::StyleColorsLight();
+
+        style.WindowPadding     = fvec2(10,10);
+        style.FramePadding      = fvec2(12,3);
+        style.ItemSpacing       = fvec2(16,6);
+        style.ItemInnerSpacing  = fvec2(6,4);
+        style.TouchExtraPadding = fvec2(0,0);
+        style.IndentSpacing     = 26;
+        style.ScrollbarSize     = 20;
+        style.GrabMinSize       = 16;
+
+        style.WindowBorderSize  = 1;
+        style.ChildBorderSize   = 1;
+        style.PopupBorderSize   = 1;
+        style.FrameBorderSize   = 1;
+        style.TabBorderSize     = 1;
+
+        style.WindowRounding    = 5;
+        style.ChildRounding     = 5;
+        style.FrameRounding     = 5;
+        style.PopupRounding     = 5;
+        style.ScrollbarRounding = 3;
+        style.GrabRounding      = 3;
+        style.TabRounding       = 3;
+
+        style.WindowTitleAlign  = fvec2(0.5,0.5);
+    }
+}
+
+namespace Data
+{
+    ReflectStruct(Procedure ,(
+        (std::string)(name),
+    ))
+}
 
 struct State
 {
@@ -21,55 +71,63 @@ struct State
 
 struct StateMain : State
 {
-    bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    bool first_tick = 1;
 
     void Tick() override
     {
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
+        Data::Procedure proc;
+        proc.name = "Проверка системы";
 
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
-        {
-            static float f = 0.0f;
-            static int counter = 0;
+        constexpr int window_flags =
+            ImGuiWindowFlags_NoResize |
+            ImGuiWindowFlags_NoMove |
+            ImGuiWindowFlags_NoCollapse |
+            ImGuiWindowFlags_NoSavedSettings |
+            ImGuiWindowFlags_HorizontalScrollbar;
 
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+        ImGui::SetNextWindowPos(ivec2(VisualOptions::outer_margin));
+        ImGui::SetNextWindowSize(window.Size() - ivec2(VisualOptions::outer_margin * 2));
 
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
+        bool keep_running = 1;
+        ImGui::Begin((proc.name + "###procedure").c_str(), &keep_running, window_flags);
+        if (!keep_running)
+            Program::Exit();
 
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+        ImGui::Columns(2, "main_columns", 1);
 
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
+        if (first_tick)
+            ImGui::SetColumnWidth(-1, iround(window.Size().x * VisualOptions::step_list_width_relative));
 
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            ImGui::End();
-        }
+        int list_column_width = ImGui::GetColumnWidth(-1);
+        if (list_column_width < VisualOptions::step_list_min_width_pixels)
+            ImGui::SetColumnWidth(-1, VisualOptions::step_list_min_width_pixels);
+        else if (int max_list_column_width = iround(VisualOptions::step_list_max_width_relative * window.Size().x); list_column_width > max_list_column_width)
+            ImGui::SetColumnWidth(-1, max_list_column_width);
 
-        // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
-        }
+        ImGui::BeginChild("step list");
+        for (int i = 0; i < 100; i++)
+            ImGui::Selectable(Str("Пункт ", i).c_str());
+        ImGui::EndChild();
+
+        ImGui::NextColumn();
+
+        ImGui::BeginChild("actions");
+        ImGui::Button("Кнопка 1");
+        ImGui::Button("Кнопка 2");
+        ImGui::EndChild();
+
+        ImGui::End();
+
+        //ImGui::ShowDemoWindow();
+
+        first_tick = 0;
     }
 };
 
 int main()
 {
     { // Initialize
-        { // Create window
+        { // Window
             ivec2 window_size(800, 600);
 
             Interface::WindowSettings window_settings;
@@ -81,13 +139,24 @@ int main()
             window = Interface::Window("Modular forms", window_size, Interface::windowed, window_settings);
         }
 
-        { // Initialize GUI
+        { // GUI
             IMGUI_CHECKVERSION();
             ImGui::CreateContext();
             ImGuiIO& io = ImGui::GetIO();
             io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
 
-            ImGui::StyleColorsDark();
+            ImFontGlyphRangesBuilder glyph_ranges_builder;
+            glyph_ranges_builder.AddRanges(io.Fonts->GetGlyphRangesDefault());
+            glyph_ranges_builder.AddRanges(io.Fonts->GetGlyphRangesCyrillic());
+            ImVector<ImWchar> glyph_ranges;
+            glyph_ranges_builder.BuildRanges(&glyph_ranges);
+
+            if (!io.Fonts->AddFontFromFileTTF("assets/Roboto-Regular.ttf", 20.0f, 0, glyph_ranges.begin()))
+                Program::Error("Unable to load font.");
+
+            ImGuiFreeType::BuildFontAtlas(io.Fonts, ImGuiFreeType::MonoHinting);
+
+            VisualOptions::GuiStyle(ImGui::GetStyle());
 
             ImGui_ImplSDL2_InitForOpenGL(window.Handle(), window.Context());
             ImGui_ImplOpenGL2_Init();
@@ -104,6 +173,8 @@ int main()
     while (1)
     {
         uint64_t delta = delta_timer();
+
+        bool imgui_frame_started = 0;
         while (metronome.Tick(delta))
         {
             window.ProcessEvents([](const SDL_Event &event)
@@ -116,14 +187,20 @@ int main()
             if (window.ExitRequested())
                 Program::Exit();
 
+            if (imgui_frame_started)
+                ImGui::EndFrame();
+
             ImGui_ImplOpenGL2_NewFrame();
             ImGui_ImplSDL2_NewFrame(window.Handle());
             ImGui::NewFrame();
+            imgui_frame_started = 1;
 
             state->Tick();
 
-            ImGui::Render();
         }
+
+        if (imgui_frame_started)
+            ImGui::Render();
 
         Graphics::Clear();
 
